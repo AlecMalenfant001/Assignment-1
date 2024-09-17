1d14edd172c5bc04187f73e10ea835985f6495f2
AlecMalenfant001 <malenfantalec@gmail.com>
2024-09-16 23:01:46 -0500

    Minor tweaks

--- Assignment01-Problem02.py
+++ Assignment01-Problem02.py
@@ -2,21 +2,19 @@
 # Assignment 01
 # Problem 02
 # This program will find the sum of the even values of the fibonacci sequence < 100000
-#import numpy as np
 
 # Initial Values
-newElement = 0
 limit = 100000 #100,000
 sequence = [0, 1] #Initialize an array with the first two values of the fibonacci sequence
+newElement = 0
 
 # Creat an sequence with the Fibonnaci Sequence
 while 1 != 0 : #infinite loop condition
-    newElement = sequence[-1] + sequence[-2] #add the most previous terms together
+    newElement = sequence[-1] + sequence[-2] #add the 2 most previous terms together
     if newElement > limit: #end loop condition
         break
     else:
         sequence.append(newElement)
-
 
 # Iterate through the sequence and find the sum of the even values
 sum = 0
@@ -29,3 +27,7 @@ for i in sequence:
 #print results
 print("Fibonnaci Sequence : " + str(sequence))
 print("Sum of even numbers : " + str(sum))
+
+# Wait for user input to kill the running program
+input("Press enter to quit ")
+quit()
